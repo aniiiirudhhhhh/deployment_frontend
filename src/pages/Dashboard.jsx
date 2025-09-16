3923b663ad6642951f1ff147a77041e73bdbc4ee
aniiiirudhhhhh <spittala@loyaltymethods.com>
2025-09-16 09:36:20 +0530

    lll

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -76,7 +76,7 @@ const Dashboard = () => {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <span className="font-medium"></span>
+                <span className="font-medium">Transparent Rewards</span>
               </div>
               <div className="flex items-center justify-center space-x-3 text-gray-700">
                 <div className="w-8 h-8 bg-purple-100 rounded-full flex items-center justify-center">
